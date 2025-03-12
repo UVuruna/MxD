49e7f0a9a174a99f3490fdd8ed74b864f949097b
UVuruna <vurunayas@gmail.com>
2025-03-12 08:18:27 +0100

    0.9

---
+++ JS/html_load.js
@@ -0,0 +1,8 @@
+function loadHTML(elementId, filePath) {
+    fetch(filePath)
+        .then(response => response.text())
+        .then(data => document.getElementById(elementId).innerHTML = data)
+        .catch(error => console.error(`Greška pri učitavanju ${filePath}:`, error));
+}
+
+loadHTML("title", "../HTML/title.html");
