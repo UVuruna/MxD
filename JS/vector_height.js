c6b4a5dcf621784d862a27f7479eacf59509b6b5
UVuruna <vurunayas@gmail.com>
2025-03-18 17:29:39 +0100

    beta

--- JS/vector_height.js
+++ JS/vector_height.js
@@ -1,28 +1,35 @@
-function getAbsoluteY (selector) {
-  var element = document.querySelector(selector)
+function getAbsoluteY(selector) {
+  var element = document.querySelector(selector);
   if (element) {
-    var rect = element.getBoundingClientRect()
-    return rect.top + window.scrollY // Apsolutna Y koordinata u odnosu na stranicu
+    var rect = element.getBoundingClientRect();
+    return rect.top + window.scrollY; // Apsolutna Y koordinata u odnosu na stranicu
   }
-  return null
+  return null;
 }
 
-function trackPositions (element1, element2, targetElement) {
-  var checkExist = setInterval(function () {
-    var y1 = getAbsoluteY(element1)
-    var y2 = getAbsoluteY(element2)
-    var target = document.querySelector(targetElement)
+function trackPositions(element1, element2, targetElement) {
+  function updateHeight() {
+    var y1 = getAbsoluteY(element1);
+    var y2 = getAbsoluteY(element2);
+    var target = document.querySelector(targetElement);
 
-    if (y1 !== null && y2 !== null) {
-      clearInterval(checkExist) // Zaustavi proveru kada pronađe elemente
-      var newHeight = Math.abs(y1 - y2) // Sprečava negativne vrednosti
-
-      target.style.height = newHeight + 'px'
-      console.log(`New height for ${targetElement}: ${newHeight}px`)
-    } else {
-      console.log('!!! Waiting for elements: ', element1, element2)
+    if (y1 !== null && y2 !== null && target) {
+      var newHeight = Math.abs(y1 - y2); // Sprečava negativne vrednosti
+      target.style.height = newHeight + 'px';
+      console.log(`Updated height for ${targetElement}: ${newHeight}px`);
     }
-  }, 50)
+  }
+
+  var checkExist = setInterval(function () {
+    if (document.querySelector(element1) && document.querySelector(element2)) {
+      clearInterval(checkExist);
+      updateHeight();
+    } else {
+      console.log('!!! Waiting for elements: ', element1, element2);
+    }
+  }, 50);
+
+  window.addEventListener('resize', updateHeight);
 }
 
-trackPositions('#dot-start', '#dot-end', '.scaling-business-vector')
+trackPositions('#dot-start', '#dot-end', '#vector');
